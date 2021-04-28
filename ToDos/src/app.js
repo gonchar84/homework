d10771ffc9b5d6a6ed4214bc80cee1a8e36d515b
Lena <gonhar84@tut.by>
2021-04-28 12:23:16 +0300

    commit

--- ToDos/src/app.js
+++ ToDos/src/app.js
@@ -9,124 +9,293 @@ const todos = [
   { name: 'task7', key: 7, status: "active" },
 ];
 
+window.todos = todos;
+
+let filter = false;
+
 // делаем запрос и вытаскиваем нужный тег
-const parent = document.querySelector("body > div > div:nth-child(4) > ul");
+let parent = document.querySelector("body > div > div:nth-child(4) > ul");
 
-// удаляем весь html в теге
-parent.innerHTML = '';
+// функция рендер списка дел
+const renderList = function () {
 
-/* <li data-id="1614959130076">
-<div class="todo">
-  <input type="checkbox" class="toggle">
-    <span>To</span>
-    <button class="destroy"></button>
-    </div><input type="text" class="edit"></li> */
+  // удаляем весь html в теге
+  parent.innerHTML = '';
 
+  let todosList = [...todos];
 
-// цикл - todos
-for (let i = 0; i < todos.length; i++) {
-  // помещаем нужный элемент в переменную
-  let todo = todos[i];
-  // создаем тег li
-  let newLi = document.createElement("li");
-  let newDiv = document.createElement("div");
-  let input = document.createElement("input");
-  let span = document.createElement("span");
-  let button = document.createElement("button");
-  newDiv.classList.add("todo")
-  input.setAttribute("type", "checkbox");
-  input.classList.add("toggle")
-  span.innerText = `${todo.name}`
-  button.classList.add("destroy")
+  // проверяем установлено ли значение фильтра
+  if (filter && filter !== 'all') {
+    // фильтруем массив по статусу возращаем только нужный список
+    todosList = todos.filter((element) => {
 
-  newDiv.append(input, span, button)
-
-  newLi.appendChild(newDiv)
-  // добавляем событие
-  newLi.onclick = function () {
-    // alert(`${todo.name}`);
-    // добавляем класс completed
-    newLi.classList.add("completed")
-  }
-  newLi.ondblclick = function () {
-
-    newLi.classList.remove("completed")
+      // проверям совпадает ли статусы
+      return element.status === filter;
+    });
   }
 
-  newLi.onmouseover = function () {
+  /*
+     let todosList2 = [];
+     for(let i=0; i < todos.length; i++) {
+       let element = todos.length;
+       if(element.status === filter) {
+         todosList2.push(element)
+       }
+     }*/
 
-    newLi.style.border = "1px solid green"
+
+  // цикл - todos
+  for (let i = 0; i < todosList.length; i++) {
+
+        let todo = todosList[i]; // помещаем задачу в переменную { name: 'task1', key: 1, status: 'overdue' }
+
+    // создаем теги
+    let li = document.createElement("li");
+    let div = document.createElement("div");
+    let input = document.createElement("input");
+    let span = document.createElement("span");
+    let button = document.createElement("button");
+
+    // создаем тег li
+    div.classList.add("todo")
+
+    // добаляем атрибут type
+    input.setAttribute("type", "checkbox");
+    input.classList.add("toggle")
+
+    // добаляем текст
+    span.innerText = `${todo.name}`
+
+    // добаляем класс
+    button.classList.add("destroy")
+
+    // всталяем сразу все элементы по очереди
+    div.append(input, span, button)
+
+    // всталяем div
+    li.appendChild(div)
+
+    // добавляем событие по клику
+    li.onclick = function () {
+      // alert(`${todo.name}`);
+
+      // добавляем класс completed
+      li.classList.add("completed")
+    }
+
+    // добавляем событие по двойному клику
+    li.ondblclick = function () {
+
+      // удаляем класс completed
+      li.classList.remove("completed")
+    }
+
+    // добавляем событие при наведение курсора
+    li.onmouseover = function () {
+
+      // добавляем рамку
+      li.style.border = "1px solid green"
+    }
+
+    // добавляем событие когда курсор уходит с элемента
+    li.onmouseout = function () {
+      // удаляем рамку
+    li.style.border = ""
+    }
+
+    // вставляем таг
+    parent.appendChild(li);
+  }
+}
+
+// рисуем список задач
+renderList();
+// рисуем бар
+renderBar();
+
+// ищем форму по аттр id
+const form = document.getElementById("insert__form");
+
+// вещаем перехват события submit
+form.onsubnmit = function (event) {
+  // сбросить действие события по умолчанию
+  event.preventDefault();
+
+  // ищем наш input, firstElementChild - первый ребенок формы
+  let input = event.currentTarget.firstElementChild;
+
+  // добавляем новое значение
+  todos.push(
+    { name: input.value, key: todos.length + 1, status: 'active' }
+  );
+
+  // делаем инпут пустым
+  input.value = '';
+
+  // рисуем список задач
+  renderList();
+  renderBar();
+}
+
+// 2 задание
+
+
+function renderBar() {
+  // находим нужный блок
+  const blockIdList = document.getElementById('js-bar');
+
+  //удалить html
+  blockIdList.innerHTML = '';
+
+  // рисуем первый блок
+  {
+    // создаем div
+    let div = document.createElement('div');
+    // добавляем класс
+    div.classList.add('col-1-4');
+    // создаем span
+    let span = document.createElement('span');
+    // добавляем класс
+    span.classList.add('total');
+    // добавляем аттрибут id
+    span.setAttribute('id', 'js-total');
+    // добавляем текст
+    span.innerText = `${todos.length} items left`;
+    // добавляем созданный тег span к тегу div
+    div.appendChild(span);
+
+    // добавляем созданный тег div к js-bar
+    blockIdList.appendChild(div);
   }
 
-  newLi.onmouseout = function () {
+  // рисуем второй блок
+  {
+    // создаем div
+    let div1 = document.createElement('div');
+    // добавляем класс
+    div1.classList.add('col-1-2');
 
-    newLi.style.border = ""
+    // создаем ul
+    let ul = document.createElement('ul');
+
+    ul.classList.add('filter');
+
+    ul.setAttribute('id', 'js-filters');
+
+    // добавляем html
+    ul.innerHTML = '<li><a href="#/all" data-status="all" class="button selected"><span>All</span></a></li>' +
+      '<li><a href="#/active" data-status="active" class="button"><span>Active</span></a></li>' +
+      '<li><a href="#/completed" data-status="done" class="button"><span>Completed</span></a></li>'
+
+    // добавляем созданный тег ul к тегу div
+    div1.appendChild(ul);
+
+    // добавляем созданный тег div к js-bar
+    blockIdList.appendChild(div1);
+  }
+  // рисуем третий блок
+  {
+    // создаем div
+    let div2 = document.createElement('div');
+    // добавляем класс
+    div2.classList.add('col-1-4');
+
+    let button = document.createElement('button');
+    button.classList.add('button');
+    button.classList.add('button--clear');
+    button.setAttribute('id', 'js-clear-completed');
+    button.innerText = 'Clear Completed';
+
+    // добавляем созданный тег button к тегу div
+    div2.appendChild(button);
+    // добавляем созданный тег div к js-bar
+    blockIdList.appendChild(div2);
   }
 
-  const form = document.getElementById("insert__form");
-  form.onsubnit = function(event){
-event.preventDefault()
-
-// console.log(event)
-  }
 
 
+  // 3 задание
 
-// todos.push(
-//   { name: event.currentTarget.firstElementChild.value, key: 7, status: "active" }
-// )
+  // ищем все ссылки
+  let links = document.querySelectorAll('#js-filters li');
+
+  // делаем цикл
+  links.forEach(function (element) {
+
+    // добавляем событыие клик
+    element.addEventListener('click', function (event) {
+
+      // ищет все a с классом selected
+      let links = document.querySelectorAll('a.selected');
+
+      // перебираем все найденные элементы
+      links.forEach(function (element) {
+
+        // удаляем класс selected у найденных элементах
+        element.classList.remove('selected');
+      });
+      // ложим currentTarget в переменную
+      let { currentTarget } = event;
 
 
-  // вставляем текст
-  // newLi.innerText = `${todo.name}`
+      // ищем первего ребенка добавляем активный класс
+      currentTarget.children[0].classList.add('selected');
+      filter = currentTarget.children[0].dataset.status;
 
-  //  вставляем тег
-  parent.appendChild(newLi);
+
+
+      //
+      renderList();
+    });
+
+  });
 
 }
 
 
 
 
-let newLi = document.createElement("li");
-
-newLi.innerText = `super first`
-
-parent.prepend(newLi)
-
-let superLast = document.createElement("li");
-
-superLast.innerText = `super last`
-
-parent.appendChild(superLast)
 
 
-let middleLast = document.createElement("li");
+// let newLi = document.createElement("li");
 
-middleLast.innerText = `middle last`
+// newLi.innerText = `super first`
 
-// вставляем перед тегом
+// parent.prepend(newLi)
 
-parent.insertBefore(middleLast, parent.children[3])
+// let superLast = document.createElement("li");
 
-parent.nextElementSibling
-parent.previousElementSibling
+// superLast.innerText = `super last`
 
-let coint = document.getElementById('js-bar')
-console.log(coint)
-// его брат выше
-console.log("previousElementSibling", coint.previousElementSibling)
+// parent.appendChild(superLast)
 
-// его брат ниже
-console.log("nextElementSibling", coint.nextElementSibling)
 
-let p = document.createElement("p");
-p.innerText = "я выше";
-parent.insertAdjacentElement("beforeBegin", p);
+// let middleLast = document.createElement("li");
 
-let p2 = document.createElement("p");
-p2.innerText = "я ниже";
-parent.insertAdjacentElement("afterEnd", p2);
+// middleLast.innerText = `middle last`
+
+// // вставляем перед тегом
+
+// parent.insertBefore(middleLast, parent.children[3])
+
+// parent.nextElementSibling
+// parent.previousElementSibling
+
+// let coint = document.getElementById('js-bar')
+// console.log(coint)
+// // его брат выше
+// console.log("previousElementSibling", coint.previousElementSibling)
+
+// // его брат ниже
+// console.log("nextElementSibling", coint.nextElementSibling)
+
+// let p = document.createElement("p");
+// p.innerText = "я выше";
+// parent.insertAdjacentElement("beforeBegin", p);
+
+// let p2 = document.createElement("p");
+// p2.innerText = "я ниже";
+// parent.insertAdjacentElement("afterEnd", p2);
 
 // заполняем html тега
 // parent.innerHTML += `<li data-id="${todo.id}">
@@ -135,34 +304,32 @@ parent.insertAdjacentElement("afterEnd", p2);
 
 
 // ищем тэг по аттр id
-let tag = document.getElementById('js-bar');
+// let tag = document.getElementById('js-bar');
 
 // ищем тэги по имени тега
-let tags = document.getElementsByTagName('div');
+// let tags = document.getElementsByTagName('div');
 
 // ищем тэги по selector запросу
-const tagsWithClassRow = document.querySelectorAll(".row");
+// const tagsWithClassRow = document.querySelectorAll(".row");
 
 
 // ищем тэг по selector запросу
-const span = document.querySelector("span");
+// const span = document.querySelector("span");
 
 // меняем его HTML, todos.length = кол во элементов в массиве
-span.innerHTML = `${todos.length} items left`;
+// span.innerHTML = `${todos.length} items left`;
 
 // ищем первый ul в документе
-const result = document.querySelector("body div ul");
+// const result = document.querySelector("body div ul");
 
 // ищем первый ul с классом list в документе
-const result2 = document.querySelector("body div ul.list");
+// const result2 = document.querySelector("body div ul.list");
 
 // ищем первый ul с классом list в документе, > означает у детей body
-const result3 = document.querySelector("body > div ul.list");
+// const result3 = document.querySelector("body > div ul.list");
 
 // ищем первый div с классом limiter в документе, nth-child - порядковый номер ребенка
-const result4 = document.querySelector("body div.limiter div:nth-child(4)");
-
-
+// const result4 = document.querySelector("body div.limiter div:nth-child(4)");
 
 const fon = document.getElementById("js-bar");
 fon.style.background = "red";
@@ -170,19 +337,6 @@ fon.style.background = "red";
 
 const color = document.querySelector("h1");
 color.style.color = "blue";
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
